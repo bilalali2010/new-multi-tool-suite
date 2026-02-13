0ae854ff6475e6d971a1a77a1d20a685ecdc320d
bilalali2010 <bilalali0905@gmail.com>
2026-02-13 23:09:25 +0500

    Remove experimental appDir configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // helps catch bugs early
-  swcMinify: true,       // faster builds with Next.js compiler
-  experimental: {
-    appDir: true,        // already using app directory
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
